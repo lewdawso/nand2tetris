38607265f6a732620f22d93346996578dd2a6809
Lewis Dawson <lewis.dawson@acano.com>
2015-12-01 14:09:11 +0000

    Update tokenizer

--- JackTokenizer.js
+++ JackTokenizer.js
@@ -1,7 +1,36 @@
 fs = require('fs');
 trimmed = [];
 parsed = [];
-finalised = [];
+tokens = [];
+
+keywords = 
+[
+    "class",
+    "constructor",
+    "function",
+    "method",
+    "field",
+    "static",
+    "var",
+    "int",
+    "char",
+    "boolean",
+    "void",
+    "true",
+    "false",
+    "null",
+    "this",
+    "let",
+    "do",
+    "if",
+    "else",
+    "while",
+    "return"
+];
+
+symbols = ["{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~"];
+
+
 
 function isComment(command) {
     if ((command[0] == "/" && command[1] == "/") || (command[0] == "/" && command[1] == "*") || command[1] == "*" || (command[1] == "*" && command[2] == "/")) {
@@ -47,22 +76,40 @@ function parse() {
     //remove whitespace
     for (var i=0; i<parsed.length; i++) {
         if (!parsed[i] == "") {
-            finalised.push(parsed[i]);
+            tokens.push(parsed[i]);
         };
     };
 };
+
+function hasMoreTokens() {
+    if(tokens.length) {
+        return true;
+    } else {
+        return false;
+    };
+}:
+
+function advance() {
+    return tokens.shift();
+}
+
+function tokenType(token) {
+    switch(token) {
+        case 
+
+    }
+
+}
 
 function main() {
     file = process.argv[2] 
     data = fs.readFileSync(file, 'ascii');
     buffer = data.split(/[\n\r\t]/);
     parse();
+    var token;
+    if (hasMoreTokens(tokens)) {
+        advance(tokens);
+    };
 }
 
 main();
-
-
-
-
-
-
