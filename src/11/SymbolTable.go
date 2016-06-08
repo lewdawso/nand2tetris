ea777dd4a4f3c9095d6ab18e4eabfabccd13b4b4
Lewis Dawson <lewis.dawson@acano.com>
2016-06-08 13:47:39 +0100

    define -> Define

--- src/11/SymbolTable.go
+++ src/11/SymbolTable.go
@@ -3,10 +3,10 @@ Symbol table abstraction. Associates identifier names found in the program
 with identifier properties needed for compilation.
 */
 
-package main
+package symtable
 
 import (
-    "fmt"
+	"fmt"
 )
 
 var classSymbolTable map[string]Symbol
@@ -14,85 +14,85 @@ var subroutineSymbolTable map[string]Symbol
 var indices map[Kind]int
 
 type Symbol struct {
-    _type string
-    kind Kind
-    index int
+	_type string
+	kind  Kind
+	index int
 }
 
 type Kind int
 
 const (
-    NONE Kind = iota
-    STATIC
-    FIELD
-    ARG
-    VAR
+	NONE Kind = iota
+	STATIC
+	FIELD
+	ARG
+	VAR
 )
 
-
 func init() {
-    classSymbolTable = make(map[string]Symbol)
-    subroutineSymbolTable = make(map[string]Symbol)
-    indices := make(map[Kind]int)
+	classSymbolTable = make(map[string]Symbol)
+	subroutineSymbolTable = make(map[string]Symbol)
+	indices := make(map[Kind]int)
 
-    indices[STATIC] = 0
-    indices[FIELD] = 0
-    indices[ARG] = 0
-    indices[VAR] = 0
+	indices[STATIC] = 0
+	indices[FIELD] = 0
+	indices[ARG] = 0
+	indices[VAR] = 0
 }
 
 func startSubroutine() {
-    subroutineSymbolTable = make(map[string]Symbol)
+	subroutineSymbolTable = make(map[string]Symbol)
 }
 
-func define(name string, _type string, kind Kind) {
-    //find the count of this kind of variable
-    index := VarCount(kind) + 1
-    indices[kind] = index
+func Define(name string, _type string, kind Kind) {
+	//find the count of this kind of variable
+	index := VarCount(kind) + 1
+	indices[kind] = index
 
-    //define Symbol variable
-    symbol :=  Symbol{_type, kind, index}
+	//define Symbol variable
+	symbol := Symbol{_type, kind, index}
 
-    if (kind == STATIC || kind == FIELD) {
-        classSymbolTable[name] = symbol
-    } else if (kind == ARG || kind == VAR)  {
-        subroutineSymbolTable[name] = symbol
-    } else {
-        fmt.Println("unrecognised kind")
-    }
+	if kind == STATIC || kind == FIELD {
+		classSymbolTable[name] = symbol
+	} else if kind == ARG || kind == VAR {
+		subroutineSymbolTable[name] = symbol
+	} else {
+		fmt.Println("unrecognised kind")
+	}
 }
 
 func VarCount(kind Kind) int {
-    return indices[kind]
+	return indices[kind]
 }
 
 func KindOf(name string) Kind {
-    if !EmptySymbol(classSymbolTable[name]) {
-        return s.kind
-    } else {
-        return subroutineSymbolTable[name].kind
-    }
+	s := classSymbolTable[name]
+	if !EmptySymbol(s) {
+		return s.kind
+	} else {
+		return subroutineSymbolTable[name].kind
+	}
 }
 
 func TypeOf(name string) string {
-    if s:= classSymbolTable[name] ; s._type != "" {
-        return s._type
-    } else {
-        return subroutineSymbolTable[name]._type
-    }
+	if s := classSymbolTable[name]; s._type != "" {
+		return s._type
+	} else {
+		return subroutineSymbolTable[name]._type
+	}
 }
 
 func IndexOf(name string) Symbol {
-    if s:= classSymbolTable[name] ; s.index == 0 {
-        return s
-    } else {
-        return subroutineSymbolTable[name]
-    }
+	if s := classSymbolTable[name]; s.index == 0 {
+		return s
+	} else {
+		return subroutineSymbolTable[name]
+	}
 }
 
 func EmptySymbol(s Symbol) bool {
-    if (s.kind == 0 && s._type == "" && s.index == 0) {
-        return true
-    }
-    return false
+	if s.kind == 0 && s._type == "" && s.index == 0 {
+		return true
+	}
+	return false
 }
